cbbad65e43208b0762e4baef05a3363461cf2714
CSeoyeon <choiseoyeon0223@gmail.com>
2022-07-25 00:32:11 +0900

    firebase 연결

--- app/src/main/java/com/example/worker/MainActivity.java
+++ app/src/main/java/com/example/worker/MainActivity.java
@@ -18,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //setContentView(R.layout.fragment_login);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
 
     }
 }
--- app/src/main/java/com/example/worker/accountAdmin/fragment/SignUpFragment.java
+++ app/src/main/java/com/example/worker/accountAdmin/fragment/SignUpFragment.java
@@ -8,18 +8,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.worker.databinding.FragmentSignupBinding;
 
 public class SignUpFragment extends Fragment {
+
+    private FragmentSignupBinding binding;
+    private NavController navController;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+       binding = FragmentSignupBinding.inflate(inflater, container, false);
+       return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+
+        navController = NavHostFragment.findNavController(SignUpFragment.this);
     }
 
 
