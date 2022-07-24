72d09516a57d10b5ec03421ab36a9fd19e13ac4d
CSeoyeon <choiseoyeon0223@gmail.com>
2022-07-25 00:45:33 +0900

    firebase 연결

--- app/src/main/java/com/example/worker/accountAdmin/fragment/SignUpFragment.java
+++ app/src/main/java/com/example/worker/accountAdmin/fragment/SignUpFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +13,14 @@ import androidx.navigation.NavController;
 import androidx.navigation.NavHostController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.worker.R;
 import com.example.worker.databinding.FragmentSignupBinding;
 
 public class SignUpFragment extends Fragment {
 
     private FragmentSignupBinding binding;
     private NavController navController;
+    private Button bt_signUp;
 
     @Nullable
     @Override
@@ -29,7 +32,17 @@ public class SignUpFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+
         navController = NavHostFragment.findNavController(SignUpFragment.this);
+        bt_signUp = binding.signUpBtSignUp;
+        bt_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_navigation_signUp_to_navigation_logIn);
+            }
+        });
+
+
     }
 
 
