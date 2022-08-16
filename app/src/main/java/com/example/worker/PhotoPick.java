3652a3e35ce98f6dd22568b63f2de69a2df4907d
Cseoyeon <choiseoyeon0223@gmail.com>
2022-08-16 15:52:05 +0900

    add user information_imageAddPick

--- app/src/main/java/com/example/worker/PhotoPick.java
+++ app/src/main/java/com/example/worker/PhotoPick.java
@@ -2,20 +2,24 @@ package com.example.worker;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 
+
 public class PhotoPick extends Activity {
 
-    public static final int PICK_IMAGE = 1;
+
+    private static final int SELECTED_PICTURE = 200;
+    private ImageButton bt_imagePick;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         this.galleryAddPic();
-
-
 
 
         finish();
@@ -23,18 +27,29 @@ public class PhotoPick extends Activity {
 
     }
 
-    public void galleryAddPic(){
+    private void galleryAddPic(){
         //get gallery image
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
 
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECTED_PICTURE);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_OK && requestCode == SELECTED_PICTURE){
+                Uri selectedImageUri = data.getData();
+                if(null != selectedImageUri){
+                    bt_imagePick.setImageURI(selectedImageUri);
+                }
+
+
+        }
     }
+
+
 }
 
