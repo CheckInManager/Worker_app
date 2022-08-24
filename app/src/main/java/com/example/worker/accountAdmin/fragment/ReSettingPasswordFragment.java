65654c27af306aab90b60402a0470c0a8940b8a0
Cseoyeon <choiseoyeon0223@gmail.com>
2022-08-24 13:35:50 +0900

    recycle adapter update

--- app/src/main/java/com/example/worker/accountAdmin/fragment/ReSettingPasswordFragment.java
+++ app/src/main/java/com/example/worker/accountAdmin/fragment/ReSettingPasswordFragment.java
@@ -1,11 +1,13 @@
 package com.example.worker.accountAdmin.fragment;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +16,14 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.worker.accountAdmin.viewModel.FindPasswordViewModel;
+import com.example.worker.R;
 import com.example.worker.accountAdmin.viewModel.ReSettingPasswordViewModel;
 import com.example.worker.databinding.FragmentResettingpasswordBinding;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+import java.util.zip.Inflater;
 
 
 public class ReSettingPasswordFragment extends Fragment {
@@ -26,6 +33,7 @@ public class ReSettingPasswordFragment extends Fragment {
     private FragmentResettingpasswordBinding binding;
     private NavController navController;
 
+    private TextView tv_alarm;
     private EditText et_password;
     private EditText et_confirmPassword;
     private Button bt_confirm;
@@ -38,9 +46,13 @@ public class ReSettingPasswordFragment extends Fragment {
         navController = NavHostFragment.findNavController(ReSettingPasswordFragment.this);
         reSettingPasswordViewModel = new ViewModelProvider(this).get(ReSettingPasswordViewModel.class);
 
+
+        //안됨!! ㅠㅠ
+        View view = inflater.inflate(R.layout.fragment_findpassword, container, true);
+        tv_alarm = binding.test;
         et_password = binding.reSettingPasswordEtPassword;
         et_confirmPassword = binding.reSettingPasswordEtConfirmpassword;
-
+        bt_confirm = view.findViewById(R.id.findPassword_bt_confirm);
 
         return binding.getRoot();
     }
@@ -49,13 +61,20 @@ public class ReSettingPasswordFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
         reSettingPasswordViewModel.updatePassword(reSettingPasswordViewModel.getCurrUser());
 
         //버튼 클릭시 -> framelayout으로 넣어서 findpasswordfragment에 있는 버튼을 써야함
 
+        bt_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_alarm.setText("hello world");
+            }
+        });
 
         String password = et_password.getText().toString();
-        String confirmPassword = et_password.getText().toString();
+        String confirmPassword = et_confirmPassword.getText().toString();
 
 
 
